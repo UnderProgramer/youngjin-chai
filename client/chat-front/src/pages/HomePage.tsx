fcdc3a605efef39b35203bae9fdfb7e69d613f0c
UnderProgramer <ajdjskq13324@gmail.com>
2025-12-27 17:13:29 +0900

    fix main page

--- client/chat-front/src/pages/HomePage.tsx
+++ client/chat-front/src/pages/HomePage.tsx
@@ -3,8 +3,8 @@ import React from "react";
 export const HomePage = () => {
     return (
         <>
-            <h1>Young Jin Gallery</h1>
-            <p>here is 영진 갤러리, 영진 very like 디시 인사이드, so i made 영진 갤러리</p>
+            <h1>test</h1>
+            <p>hello world</p>
         </>
     )
 }
