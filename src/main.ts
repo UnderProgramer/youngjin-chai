c6a5226e031426d5517b37fb1d188c57e652be30
UnderProgramer <ajdjskq13324@gmail.com>
2026-03-27 01:08:30 +0900

    add cors *

--- src/main.ts
+++ src/main.ts
@@ -15,10 +15,7 @@ async function bootstrap() {
   }))
 
   app.enableCors({
-    origin: [
-      'http://localhost:5173',
-      'http://localhost:5500',
-    ],
+    origin: '*',
     credentials: true,
   });
 
