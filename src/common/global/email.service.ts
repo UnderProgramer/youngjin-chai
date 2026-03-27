b36aba61505995fb52aa15834d8e68136463a77e
UnderProgramer <ajdjskq13324@gmail.com>
2026-03-27 11:32:01 +0900

    fix user service

--- src/common/global/email.service.ts
+++ src/common/global/email.service.ts
@@ -35,7 +35,7 @@ export class EmailService {
         try {
             return await this.transporter.sendMail(emailOptions)
         } catch (e){
-            throw new BadRequestException(`[ERROR] : your email? : email \n${e}`)
+            throw new BadRequestException(`[ERROR] : your email? : ${email}\n code : ${code} \n${e}`)
         }
     }
 }
